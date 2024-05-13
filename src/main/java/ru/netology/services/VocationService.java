d3b9937245120448f120c578c1e9ef0509b7c88c
Daria Sakulina <evon620c@bk.ru>
2024-05-13 22:12:05 +0300

    reformat code

--- src/main/java/ru/netology/services/VocationService.java
+++ src/main/java/ru/netology/services/VocationService.java
@@ -2,17 +2,17 @@ package ru.netology.services;
 
 public class VocationService {
 
- public int calculate(int income, int expenses, int threshold) {
-       int count = 0; // счётчик месяцев отдыха
-       int money = 0; // количество денег на счету
-     for (int month = 1; month < 13; month++) {
-          if (money >= threshold) { // можем ли отдыхать?
-            count++; // увеличиваем счётчик месяцев отдыха
-               money = (money - expenses) / 3;
-          } else {
-               money = money + income - expenses;
-           }
-       }
+    public int calculate(int income, int expenses, int threshold) {
+        int count = 0; // счётчик месяцев отдыха
+        int money = 0; // количество денег на счету
+        for (int month = 1; month < 13; month++) {
+            if (money >= threshold) { // можем ли отдыхать?
+                count++; // увеличиваем счётчик месяцев отдыха
+                money = (money - expenses) / 3;
+            } else {
+                money = money + income - expenses;
+            }
+        }
         return count;
     }
 
